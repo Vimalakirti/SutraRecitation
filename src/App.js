bbd05abf92b7adb49c6758b1a39b0525c8287308
Vimalakirti <r08942078@ntu.edu.tw>
2021-05-04 22:38:52 +0800

    more sutras

--- src/App.js
+++ src/App.js
@@ -1,95 +1,146 @@
 import React, {useState} from 'react'
+import { Dropdown } from 'semantic-ui-react'
 import ed from 'edit-distance'
-import {sutra, chapter} from './sutra'
+import {diamond_sutra, heart_sutra, universal_gate_chapter, bodhisattva_samantabhadra} from './sutra'
+
+const sutraOptions = [
+    {
+      key: 'DiamondSutra',
+      text: '金剛經',
+      value: 'DiamondSutra',
+    },
+    {
+      key: 'HeartSutra',
+      text: '心經',
+      value: 'HeartSutra',
+    },
+    {
+      key: 'BodhisattvaSamantabhadra',
+      text: '普賢行願品',
+      value: 'BodhisattvaSamantabhadra',
+    },
+    {
+      key: 'TheUniversalGate',
+      text: '普門品',
+      value: 'TheUniversalGate',
+    }
+]
 
 const App = () => {
-    const regex = /[。，、；：？！「」『』:punct:!?., ]/g
+    const regex = /[　\n。，、；：？！「」『』:punct:!?., ]/g
     const [index, setIndex] = useState(1)
     const [recitation, setRecitation] = useState("")
     const [show, setShow] = useState(true)
     const [showAnswer, setShowAnswer] = useState(false)
-    
+
+    const [currentSutra, setCurrentSutra] = useState(diamond_sutra)
+    const handleDropdown = (e, data) => {
+        switch(data.value){
+            case "DiamondSutra":
+                setIndex(1)
+                setCurrentSutra(diamond_sutra)
+            break
+            case "HeartSutra":
+                setIndex(1)
+                setCurrentSutra(heart_sutra)
+            break
+            case "TheUniversalGate":
+                setIndex(1)
+                setCurrentSutra(universal_gate_chapter)
+            break
+            case "BodhisattvaSamantabhadra":
+                setIndex(1)
+                setCurrentSutra(bodhisattva_samantabhadra)
+            break
+            default:
+                setIndex(1)
+                setCurrentSutra(diamond_sutra)
+            break
+        }
+    }
+
     var insert, remove, update;
     insert = remove = function(node) { return 1; }
     update = function(stringA, stringB) { return stringA !== stringB ? 1 : 0; }
-    var lev = ed.levenshtein(sutra[index-1].replace(regex,''), recitation.replace(regex,''), insert, remove, update);
+    var lev = ed.levenshtein(currentSutra.sutra[index-1].replace(regex,''), recitation.replace(regex,''), insert, remove, update);
     console.log('Levenshtein', lev.distance, lev.pairs(), lev.alignment())
 
     return (
-        <div class="ui container">
+        <div className="ui container">
             <br/>
-            <h1 class="ui header">金剛般若波羅蜜經</h1>
-            <p>姚秦天竺三藏鳩摩羅什 譯</p>
+            <Dropdown placeholder="請選擇經文" fluid selection options={sutraOptions} onChange={handleDropdown}/>
+            <h1 className="ui header">{currentSutra.sutraName}</h1>
+            <p>{currentSutra.translator}譯</p>
             {index>1?
-            <button class="ui right labeled icon button" onClick={()=>{setIndex(index-1)}}>
-                <i class="left arrow icon"></i>
+            <button className="ui right labeled icon button" onClick={()=>{setIndex(index-1)}}>
+                <i className="left arrow icon"></i>
                 Prev
             </button>
             :
             <div/>
             }
-            {index<32?
-            <button class="ui right labeled icon button" onClick={()=>{setIndex(index+1)}}>
-                <i class="right arrow icon"></i>
+            {index<currentSutra.numChapter?
+            <button className="ui right labeled icon button" onClick={()=>{setIndex(index+1)}}>
+                <i className="right arrow icon"></i>
                 Next
             </button>
             :
             <div></div>
             }
             {show?
-            <button class="ui black button" onClick={()=>{setShow(!show)}}>
-                <i class="eye slash icon"></i>
+            <button className="ui black button" onClick={()=>{setShow(!show)}}>
+                <i className="eye slash icon"></i>
                 隱藏經文
             </button>
             :
-            <button class="ui green button" onClick={()=>{setShow(!show)}}>
-                <i class="eye icon"></i>
+            <button className="ui green button" onClick={()=>{setShow(!show)}}>
+                <i className="eye icon"></i>
                 顯示經文
             </button>
             }
-            <h2 class="ui header">{chapter[index-1]}</h2>
+            <h2 className="ui header">{currentSutra.chapter[index-1]}</h2>
             {show?
-            <h3 class="ui header">{sutra[index-1]}</h3>
+            <h3 className="ui header">{currentSutra.sutra[index-1]}</h3>
             :
             <div/>
             }
             
-            <div class="ui divider"></div>
-            <div class="ui form">
-                <div class="field">
-                    <label><i class="edit icon"></i>背誦區：
+            <div className="ui divider"></div>
+            <div className="ui form">
+                <div className="field">
+                    <label><i className="edit icon"></i>背誦區：
                     </label>
                     <textarea onChange={(e)=>{setRecitation(e.target.value)}}></textarea>
-                    <label><i class="microphone icon"></i>小提醒：可以用手機的語音輸入法來默背哦
+                    <label><i className="microphone icon"></i>小提醒：可以用手機的語音輸入法來默背哦
                     </label>
                 </div>
             </div>
-            <div class="ui divider"></div>
+            <div className="ui divider"></div>
             {showAnswer?
             <div>
-            <button class="ui black button" onClick={()=>{setShowAnswer(!showAnswer)}}>
-                <i class="eye slash icon"></i>
+            <button className="ui black button" onClick={()=>{setShowAnswer(!showAnswer)}}>
+                <i className="eye slash icon"></i>
                 隱藏答案
             </button>
-                <h3 class="ui header">待修改的字數：{lev.distance}</h3>
-                <table class="ui celled table">
+                <h3 className="ui header">待修改的字數：{lev.distance}</h3>
+                <table className="ui celled table">
                 <thead>
                     <tr>
                     <th></th>
-                    <th class="positive">正確答案</th>
-                    <th class="warning">原始作答</th>
+                    <th className="positive">正確答案</th>
+                    <th className="warning">原始作答</th>
                 </tr></thead>
                 <tbody>
                     {lev.pairs().map(function(name, index){
                     return (
                         <tr>
-                            {name[0]==name[1]?
-                            <i class='icon big green check'></i>
+                            {name[0]===name[1]?
+                            <i className='icon big green check'></i>
                             :
-                            <i class='icon big red times'></i>
+                            <i className='icon big red times'></i>
                             }
-                            <td class="positive">{name[0]}</td>
-                            <td class="warning">{name[1]}</td>
+                            <td className="positive">{name[0]}</td>
+                            <td className="warning">{name[1]}</td>
                         </tr>
                     );
                     })}
@@ -98,13 +149,13 @@ const App = () => {
             </div>
             
             :
-            <button class="ui blue button" onClick={()=>{setShowAnswer(!showAnswer)}}>
-                <i class="eye icon"></i>
+            <button className="ui blue button" onClick={()=>{setShowAnswer(!showAnswer)}}>
+                <i className="eye icon"></i>
                 顯示答案
             </button>
             }
             
-            <div class="ui divider"></div>
+            <div className="ui divider"></div>
             <h6>歡迎任何建議：chenbingjyue@gmail.com</h6>
         </div>
     )
